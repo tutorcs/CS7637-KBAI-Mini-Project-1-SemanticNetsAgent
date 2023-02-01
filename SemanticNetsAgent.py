8a88e27907c2ac01bbbe9524d9325a509bae945c
tutorcs <tutorcs@163.com>
2023-02-01 12:35:22 +0800

    WeChat cstutorcs CS7637 KBAI Mini Project 1 SemanticNetsAgent

---
+++ SemanticNetsAgent.py
@@ -0,0 +1,21 @@
+https://tutorcs.com
+WeChat: cstutorcs
+QQ: 749389476
+Email: tutorcs@163.com
+class SemanticNetsAgent:
+    def __init__(self):
+        #If you want to do any initial processing, add it here.
+        pass
+
+    def solve(self, initial_sheep, initial_wolves):
+        #Add your code here! Your solve method should receive
+        #the initial number of sheep and wolves as integers,
+        #and return a list of 2-tuples that represent the moves
+        #required to get all sheep and wolves from the left
+        #side of the river to the right.
+        #
+        #If it is impossible to move the animals over according
+        #to the rules of the problem, return an empty list of
+        #moves.
+        pass
+
---
+++ main.py
@@ -0,0 +1,21 @@
+https://tutorcs.com
+WeChat: cstutorcs
+QQ: 749389476
+Email: tutorcs@163.com
+from SemanticNetsAgent import SemanticNetsAgent
+
+def test():
+    #This will test your SemanticNetsAgent
+	#with seven initial test cases.
+    test_agent = SemanticNetsAgent()
+
+    print(test_agent.solve(1, 1))
+    print(test_agent.solve(2, 2))
+    print(test_agent.solve(3, 3))
+    print(test_agent.solve(5, 3))
+    print(test_agent.solve(6, 3))
+    print(test_agent.solve(7, 3))
+    print(test_agent.solve(5, 5))
+
+if __name__ == "__main__":
+    test()
